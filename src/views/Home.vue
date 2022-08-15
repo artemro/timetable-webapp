d44d19ac2d2dbd2049c151b27df6f92014b0309f
Roman Dyakov <dyakovri@gmail.com>
2022-08-15 22:20:37 +0300

    Draft for timetable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,793 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <table class="hTable">
+    <tbody>
+      <tr class="hide">
+        <td colspan="2" class="hTop hImg">
+          <a href="/"><img src="/img/phys.png" /></a>
+        </td>
+        <td class="hTop">&nbsp;РАСПИСАНИЕ ФИЗИЧЕСКОГО ФАКУЛЬТЕТА МГУ</td>
+      </tr>
+      <tr class="tdheader">
+        <td colspan="2" align="right" valign="bottom">
+          <a href="/table"><b>1 / 1</b></a>
+        </td>
+        <td align="center" valign="bottom">
+          <b
+            ><a href="/table/1/1.htm"><b>102</b></a></b
+          >
+        </td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td rowspan="6"><span class="weekday">ПОНЕДЕЛЬНИК</span></td>
+        <td class="tdtime">9:00<br />- - -&nbsp;&nbsp;<br />10:35</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">10:50<br />- - -&nbsp;&nbsp;<br />12:25</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">13:30<br />- - -&nbsp;&nbsp;<br />15:05</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">15:20<br />- - -&nbsp;&nbsp;<br />16:55</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">17:05<br />- - -&nbsp;&nbsp;<br />18:40</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">18:55<br />- - -&nbsp;&nbsp;<br />20:30</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td rowspan="6"><span class="weekday">ВТОРНИК</span></td>
+        <td class="tdtime">9:00<br />- - -&nbsp;&nbsp;<br />10:35</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">10:50<br />- - -&nbsp;&nbsp;<br />12:25</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">13:30<br />- - -&nbsp;&nbsp;<br />15:05</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">15:20<br />- - -&nbsp;&nbsp;<br />16:55</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">17:05<br />- - -&nbsp;&nbsp;<br />18:40</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">18:55<br />- - -&nbsp;&nbsp;<br />20:30</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td rowspan="6"><span class="weekday">СРЕДА</span></td>
+        <td class="tdtime">9:00<br />- - -&nbsp;&nbsp;<br />10:35</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">10:50<br />- - -&nbsp;&nbsp;<br />12:25</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">13:30<br />- - -&nbsp;&nbsp;<br />15:05</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">15:20<br />- - -&nbsp;&nbsp;<br />16:55</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">17:05<br />- - -&nbsp;&nbsp;<br />18:40</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">18:55<br />- - -&nbsp;&nbsp;<br />20:30</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td rowspan="6"><span class="weekday">ЧЕТВЕРГ</span></td>
+        <td class="tdtime">9:00<br />- - -&nbsp;&nbsp;<br />10:35</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">10:50<br />- - -&nbsp;&nbsp;<br />12:25</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">13:30<br />- - -&nbsp;&nbsp;<br />15:05</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">15:20<br />- - -&nbsp;&nbsp;<br />16:55</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">17:05<br />- - -&nbsp;&nbsp;<br />18:40</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">18:55<br />- - -&nbsp;&nbsp;<br />20:30</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td rowspan="6"><span class="weekday">ПЯТНИЦА</span></td>
+        <td class="tdtime">9:00<br />- - -&nbsp;&nbsp;<br />10:35</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">10:50<br />- - -&nbsp;&nbsp;<br />12:25</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">13:30<br />- - -&nbsp;&nbsp;<br />15:05</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">15:20<br />- - -&nbsp;&nbsp;<br />16:55</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">17:05<br />- - -&nbsp;&nbsp;<br />18:40</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">18:55<br />- - -&nbsp;&nbsp;<br />20:30</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td rowspan="6"><span class="weekday">СУББОТА</span></td>
+        <td class="tdtime">9:00<br />- - -&nbsp;&nbsp;<br />10:35</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">10:50<br />- - -&nbsp;&nbsp;<br />12:25</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">13:30<br />- - -&nbsp;&nbsp;<br />15:05</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">15:20<br />- - -&nbsp;&nbsp;<br />16:55</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">17:05<br />- - -&nbsp;&nbsp;<br />18:40</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="tdtime">18:55<br />- - -&nbsp;&nbsp;<br />20:30</td>
+        <td align="center" class="tditem1">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="delimiter" colspan="3">&nbsp;</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style scoped>
+body {
+  color: #202020;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: normal;
+  font-size: 1.1em;
+  text-decoration: none;
+  margin: 5px;
+}
+
+a:link {
+  color: inherit;
+  text-decoration: inherit;
+}
+a:visited {
+  color: inherit;
+}
+a:hover {
+  color: inherit;
+  text-decoration: inherit;
+}
+a:active {
+  color: inherit;
+}
+
+p,
+ul {
+  margin-bottom: 20px;
+  line-height: 1.6em;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-size: 1.5em;
+  color: #000;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+h1 {
+  font-size: 2em;
+  padding-bottom: 0;
+  margin-bottom: 0;
+}
+h2 {
+  padding: 0 0 10px 0;
+}
+h3 {
+  font-size: 1.2em;
+}
+h4 {
+  font-size: 1.1em;
+}
+h5,
+h6 {
+  font-size: 1em;
+}
+
+.rightColumn > h1,
+.rightColumn > h2,
+.leftColumn > h1,
+.leftColumn > h2 {
+  margin-top: 0;
+}
+
+#header {
+  position: relative;
+  margin-bottom: 0px;
+  color: #000;
+  padding: 0;
+}
+
+#header h1 {
+  font-weight: bold;
+  padding: 5px 0;
+  margin: 0;
+  color: #fff;
+  border: none;
+  line-height: 2em;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2em !important;
+}
+
+#main {
+  padding: 10px 30px 15px 30px;
+  background-color: #fff;
+  margin-bottom: 30px;
+  _height: 1px;
+}
+
+#footer {
+  color: #999;
+  padding: 10px 0;
+  text-align: center;
+  line-height: normal;
+  margin: 0;
+  font-size: 0.9em;
+}
+
+ul#menu {
+  border-bottom: 1px #5c87b2 solid;
+  padding: 0 0 2px;
+  position: relative;
+  margin: 0;
+  text-align: right;
+}
+
+ul#menu li {
+  display: inline;
+  list-style: none;
+}
+
+ul#menu li#greeting {
+  padding: 10px 20px;
+  font-weight: bold;
+  text-decoration: none;
+  line-height: 2.8em;
+  color: #fff;
+}
+
+ul#menu li a {
+  padding: 10px 20px;
+  font-weight: bold;
+  text-decoration: none;
+  line-height: 2.8em;
+  background-color: #e8eef4;
+  color: #034af3;
+}
+
+ul#menu li a:hover {
+  background-color: #fff;
+  text-decoration: none;
+}
+
+ul#menu li a:active {
+  background-color: #a6e2a6;
+  text-decoration: none;
+}
+
+ul#menu li.selected a {
+  background-color: #fff;
+  color: #000;
+}
+
+fieldset {
+  margin: 1em 0;
+  padding: 1em;
+  border: 1px solid #ccc;
+}
+
+fieldset p {
+  margin: 2px 12px 10px 10px;
+}
+
+fieldset label {
+  display: block;
+}
+
+legend {
+  font-size: 1.1em;
+  font-weight: 600;
+  padding: 2px 4px 8px 4px;
+}
+
+#logindisplay a:link {
+  color: white;
+  text-decoration: underline;
+}
+
+#logindisplay a:visited {
+  color: white;
+  text-decoration: underline;
+}
+
+#logindisplay a:hover {
+  color: white;
+  text-decoration: none;
+}
+
+.modalBackground {
+  background-color: Gray;
+  filter: alpha(opacity=70);
+  opacity: 0.7;
+}
+
+.modalPopup {
+  background-color: #ffffdd;
+  border-width: 3px;
+  border-style: solid;
+  border-color: Gray;
+  padding: 3px;
+  width: 250px;
+}
+
+.main {
+  font-size: 1em;
+  font-weight: bold;
+}
+.login {
+  width: 100px;
+}
+.intext {
+  width: 250px;
+}
+.innum {
+  width: 50px;
+}
+.list {
+  font-size: 0.8em;
+  font-weight: normal;
+}
+.list li {
+  display: inline-block;
+  background-color: #e8eef4;
+  font-weight: normal;
+  margin: 1px;
+  padding: 1px;
+  line-height: 16px;
+  border: dached 1px #ff0000;
+  text-decoration: none;
+  cursor: default;
+}
+.list li:hover {
+  background-color: #ffff00;
+  cursor: pointer;
+}
+.selectIndex {
+  padding: 3px;
+  font-size: 0.9em;
+  text-decoration: none;
+  font-weight: bold;
+  color: #ffffff;
+  background-color: #202020;
+}
+.unselectIndex {
+  padding: 3px;
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 0.9em;
+  color: #0033cc;
+  background-color: #e0e0f0;
+}
+.unselectIndex:hover {
+  background-color: #e0e040;
+  cursor: pointer;
+}
+
+.tbl {
+  font-size: 0.9em;
+  font-weight: normal;
+}
+.bgeven {
+  background-color: #e0e0e0;
+}
+.bgodd {
+  background-color: #f0f0f0;
+}
+.menu {
+  background-color: #e0e0f0;
+  text-decoration: none;
+  font-family: Arial;
+  font-size: 0.9em;
+  color: #0033cc;
+  font-weight: bold;
+}
+.menutd:hover {
+  background-color: #e0e080;
+  cursor: pointer;
+}
+.menuhelp {
+  color: #804000;
+  font-weight: normal;
+  font-style: italic;
+}
+.menuhelp:hover {
+  background-color: #e0e080;
+  cursor: pointer;
+}
+.menuactive {
+  background-color: #001030;
+  text-decoration: none;
+  font-family: Arial;
+  font-size: 0.9em;
+  color: #ffffff;
+  font-weight: bold;
+  cursor: default;
+}
+.dataTime {
+  width: 50px;
+}
+.dataNum {
+  width: 30px;
+}
+.dataText {
+  width: 120px;
+}
+.dataTextLeft {
+  width: 400px;
+}
+.dataTextLong {
+  width: 600px;
+}
+
+.mtable {
+  border: dashed 1px #000010;
+  color: #222222;
+}
+.point {
+  cursor: pointer;
+}
+.tdplan {
+  cursor: pointer;
+}
+.td0 {
+  background-color: #e0e0e0;
+  cursor: pointer;
+}
+.td1 {
+  background-color: #f0f0f0;
+  cursor: pointer;
+}
+.tn0 {
+  background-color: #e0e0e0;
+}
+.tn1 {
+  background-color: #f0f0f0;
+}
+.tdsection {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 1em;
+  color: White;
+  background-color: Blue;
+}
+.tdbusy {
+  font-family: Arial;
+  font-weight: normal;
+  font-size: 0.8em;
+}
+.tdroom {
+  font-family: Arial;
+  font-weight: normal;
+  font-size: 0.8em;
+  cursor: pointer;
+}
+.tdtime {
+  text-align: right;
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 12px;
+  cursor: pointer;
+  height: 2em;
+}
+.tfooter {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 0.9em;
+}
+.tditem1 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tditem2 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tditem3 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tditem4 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tditem5 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tditem6 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tditem7 {
+  font-family: Arial;
+  font-size: 1.1em;
+}
+.tditem8 {
+  font-family: Arial;
+  font-size: 1.1em;
+}
+.tditem9 {
+  font-family: Arial;
+  font-size: 1.1em;
+}
+.tditem10 {
+  font-family: Arial;
+  font-size: 1.1em;
+}
+.tditem11 {
+  font-family: Arial;
+  font-size: 1.1em;
+}
+.tditem12 {
+  font-family: Arial;
+  font-size: 1.1em;
+}
+.tdsmall0 {
+  font-family: Arial;
+  font-size: 0.8em;
+}
+.tdsmall1 {
+  font-family: Arial;
+  font-size: 0.8em;
+}
+.tdsmall2 {
+  font-family: Arial;
+  font-size: 0.8em;
+}
+.tdsmall3 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tdsmall4 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tdsmall5 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tdsmall6 {
+  font-family: Arial;
+  font-size: 0.9em;
+}
+.tdsmall7 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tdsmall8 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tdsmall9 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tdsmall10 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tdsmall11 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.tdsmall12 {
+  font-family: Arial;
+  font-size: 1em;
+}
+.error {
+  font-family: Arial;
+  font-size: 12px;
+  color: #ee2222;
+  font-weight: bold;
+}
+.topleft {
+  cursor: pointer;
+  background-color: Gray;
+  color: White;
+  font-family: Courier New;
+  font-size: 1.1em;
+  font-weight: bold;
+}
+.topleftday {
+  cursor: pointer;
+  background-color: Red;
+  color: White;
+  font-family: Courier New;
+  font-size: 1.1em;
+  font-weight: bold;
+}
+.topleftbusy {
+  cursor: pointer;
+  background-color: Green;
+  color: White;
+  font-family: Courier New;
+  font-size: 1.1em;
+  font-weight: bold;
+}
+.thead0 {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 1em;
+}
+.thead1 {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+.thead2 {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 2em;
+}
+.thead3 {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 2.2em;
+}
+.thead {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+.tfooter {
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 0.9em;
+}
+
+.dayNow {
+  background-color: #8080ff;
+}
+.dayReserve {
+  background-color: #80ff80;
+}
+.dayNormal {
+  background-color: #f0f0f0;
+}
+.dayLesson {
+  background-color: #f0f080;
+}
+.dayBusy {
+  background-color: #f08080;
+}
+.delimiter {
+  font-family: Arial;
+  font-size: 0.4em;
+  background-color: #e4e4e4;
+}
+.tdheader,
+.hDiv {
+  background-color: #606060;
+  color: white;
+  font-weight: bold;
+  padding: 0.5em;
+  text-align: center;
+}
+.hTop,
+.hImg {
+  color: #808080;
+  background-color: white;
+  font-weight: bold;
+  padding: 0.5em;
+  text-align: left;
+}
+.hImg {
+  width: 3em !important;
+}
+.hImg img {
+  height: 2em;
+}
+.hTable {
+  width: 100%;
+  border-collapse: collapse;
+}
+.hTable td {
+  border: 1px #c0c0c0 solid;
+  padding: 0.3em;
+}
+.hTable td td {
+  border: 0 none;
+  padding: 0;
+}
+.hCourse {
+  color: #606060;
+  font-weight: bold;
+  padding-left: 2em !important;
+}
+
+tr.hide {
+  opacity: 0;
+  height: 0;
+  overflow: hidden;
+}
+
+span.weekday {
+  writing-mode: vertical-rl;
+  text-orientation: mixed;
+}
+</style>
