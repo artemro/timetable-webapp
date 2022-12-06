bc1836a59c61a86324a6b59dad2fbfa763476059
artemro <artem.rozhdestvenskiy@gmail.com>
2022-12-07 00:38:43 +0300

    поменял местами иконки

--- src/components/GroupRow.vue
+++ src/components/GroupRow.vue
@@ -1,10 +1,10 @@
 <template>
   <li class="event-item noselect" v-for="item in 2" :key="item.id">
     <div class="event-item-icon" v-if="item == 1">
-      <span class="material-symbols-sharp">schedule</span>
+      <span class="material-symbols-sharp">group</span>
     </div>
     <div class="event-item-icon" v-else>
-      <span class="material-symbols-sharp">group</span>
+      <span class="material-symbols-sharp">schedule</span>
     </div>
     <div class="event-item-information">
       <div v-if="item == 1">
